e0058d41fa67219214bcba340da065020bcc15b8
Charles Newman <chaz.new96@gmail.com>
2022-03-02 11:20:24 +0000

    Populated table view with a list of employees

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Employees {
+    let firstName:String
+    let lastName:String
+    let age:Int
+    let company:String
+    
+}
 
+class EmployeeCell: UITableViewCell {
+    @IBOutlet weak var fNameLbl: UILabel!
+    @IBOutlet weak var lNameLbl: UILabel!
+    @IBOutlet weak var ageLbl: UILabel!
+    @IBOutlet weak var companyLbl: UILabel!
+}
+
+class ViewController: UIViewController, UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var employee:[Employees] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        populateList()
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return employee.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tblViewCell", for: indexPath) as! EmployeeCell
+        cell.fNameLbl.text = "\(employee[indexPath.row].firstName)"
+        cell.lNameLbl.text = "\(employee[indexPath.row].lastName)"
+        cell.ageLbl.text = "\(employee[indexPath.row].age)"
+        cell.companyLbl.text = "\(employee[indexPath.row].company)"
+        return cell
+    }
+    
+    func populateList() {
+        let e1 = Employees(firstName:"David", lastName: "Taylor", age: 24, company: "Google")
+        let e2 = Employees(firstName: "Nathan", lastName: "Kwok", age: 24, company: "Facebook")
+        let e3 = Employees(firstName: "Charles", lastName: "Newman", age: 25, company: "ITC")
+        let e4 = Employees(firstName: "Robert", lastName: "Ryan", age: 24, company: "Yahoo")
+        let e5 = Employees(firstName: "James", lastName: "Hall", age: 39, company: "NetWatch")
+        
+        employee = [e1, e2, e3, e4, e5]
+    }
+    
 }
 
